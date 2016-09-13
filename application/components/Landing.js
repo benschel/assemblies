cb709fa9df67f0ae473b1fe47b82b623f23cda3e
Ben <benschel170@gmail.com>
2016-09-12 21:28:51 -0400

    Styled landing screen

--- application/components/Landing.js
+++ application/components/Landing.js
@@ -1,13 +1,14 @@
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import React, { Component } from 'react';
 import {
     View,
     Text,
+    Image,
     TouchableOpacity
 } from 'react-native';
 
-import Icon from 'react-native-vector-icons/Ionicons';
-import NavigationBar from 'react-native-navbar';
 import Colors from '../styles/colors';
+import { landingStyles as styles } from '../styles';
 import { globals } from '../styles';
 
 
@@ -24,25 +25,35 @@ class Landing extends Component {
     }
 
     render() {
-        let titleConfig = {
-            title: 'Landing',
-            tintColor: 'white'
-        };
-
         return (
-            <View style={globals.flexContainer}>
-                <NavigationBar
-                    title={titleConfig}
-                    tintColor={Colors.brandPrimary}
-                />
-                <View style={globals.flexCenter}>
-                    <Text style={globals.h2}>
-                        This is the Landing Page
-                    </Text>
-                    <TouchableOpacity onPress={this.visitDashboard}>
-                        <Text>Go to the Dashboard</Text>
-                    </TouchableOpacity>
+            <View style={styles.container}>
+                <View style={styles.container}>
+                    <Image
+                        style={styles.backgroundImage}
+                        source={require('../assets/images/welcome.png')}
+                    />
                 </View>
+                <View style={globals.flexCenter}>
+                    <Image
+                        style={styles.logo}
+                        source={require('../assets/images/logo.png')}
+                    />
+                    <Text style={[globals.lightText, globals.h2, globals.mb2]}>
+                        assemblies
+                    </Text>
+                    <Text style={[globals.lightText, globals.h4]}>
+                        Where Developers Connect
+                    </Text>
+                </View>
+                <TouchableOpacity
+                    style={globals.button}
+                    onPress={this.visitDashboard}
+                >
+                    <Icon name='person' size={36} color='white' />
+                    <Text style={globals.buttonText}>
+                        Go to Dashboard
+                    </Text>
+                </TouchableOpacity>
             </View>
         );
     }
